a0b519be0197c30710717bde73d1247b56ff21e8
Yunai39 <yunai39@gmail.com>
2015-03-12 22:56:08 +0100

    On login if ajax return true or false

--- src/Arch/Security/Controller/SecurityController.php
+++ src/Arch/Security/Controller/SecurityController.php
@@ -2,6 +2,7 @@
 namespace Arch\Security\Controller;
 
 
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Arch\Controller;
 
@@ -22,11 +23,16 @@ class SecurityController extends Controller{
 				$user = $userProvider->authentificate($username,$password,$this->configuration['security']['user_encoder']);
 				
                                 $this->session->setUser($user);
-                                
+                                if($request->isXmlHttpRequest()){
+                                    return JsonResponse('true');
+                                }
                                 return $this->redirect('home');
 			}catch(\Exception $e){
 				$this->session->getFlashBag()->add('login_error',$e->getMessage());
                                 return $this->redirect('login');
+                                if($request->isXmlHttpRequest()){
+                                    return JsonResponse('false');
+                                }
 			}
 		}
 	}
